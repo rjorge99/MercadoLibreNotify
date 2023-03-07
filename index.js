6d3e799e061158009e56f9f742e4c7a58bb2a2b7
Jorge Reyes <rjorge99@hotmail.com>
2023-03-07 10:46:51 -0600

    Commin inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.use(express.json());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('App is running');
+});
+
+router.post('/notifications', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    res.status(200).send(JSON.stringify(req.body));
+});
+
+app.listen(5000, () => {
+    console.log('App is running');
+});
+
+module.exports = app;
