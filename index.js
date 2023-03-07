1065018df70bd6d9d1ed912684b5b5567b7f7e47
Jorge Reyes <rjorge99@hotmail.com>
2023-03-07 10:50:25 -0600

    asoehu

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 
+app.use(express.static('public'));
 app.use(express.json());
 
 const router = express.Router();
