3a263450f5d14ad3e39c58bf2b82ca616a522eaf
Jorge Reyes <rjorge99@hotmail.com>
2023-03-07 11:21:46 -0600

    aosehu

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const serverless = require('serverless-http');
+const cors = require('cors');
 const app = express();
 
-app.use(express.static('public'));
+app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 const router = express.Router();
 
@@ -15,6 +16,8 @@ router.post('/notifications', (req, res) => {
     console.log(JSON.stringify(req.body));
     res.status(200).send(JSON.stringify(req.body));
 });
+
+app.use('/', router);
 
 app.listen(5000, () => {
     console.log('App is running');
